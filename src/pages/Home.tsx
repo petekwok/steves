95e94c6f0e0765fe6a4ebd4c96a5aa73f1058404
petekwok <pete.kwok@gmail.com>
2022-11-30 14:35:51 +1100

    Added deliver options on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,6 +47,56 @@ const Home: React.FC<{}> = () => {
                 </div>
               </div>
             </div>
+            <div className="container p-4">
+              <div className="row justify-content-center">
+                <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb-3">
+                  <div className="border deliver border-5 white ">
+                    <a
+                      target="_blank"
+                      href="https://www.ubereats.com/gb/store/steves-fish-%26-chips/ZR__MpsRXVCoesanXiJCaQ/?utm_campaign=place-action-link&utm_medium=organic&utm_source=google"
+                    >
+                      <div className="loc_pic_sq deliveroo d-flex flex-column align-items-center gap-2 p-3">
+                        <div className="icondiv d-flex align-items-center justify-content-center">
+                          <img
+                            alt="deliveroo"
+                            data-src="../images/uber-eats.png"
+                            className="deliver-logo"
+                            src={require('../images/uber-eats.png')}
+                          />
+                        </div>
+                        <h2 className="people-box-sub-title">Uber eats</h2>
+                        <div className="order-btn border deliver border-2 white p-2">
+                          Order now
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb-3">
+                  <div className="border deliver border-5 white ">
+                    <a
+                      target="_blank"
+                      href="https://www.just-eat.co.uk/restaurants-steves-fish-and-chips-worthing/menu?utm_source=google&utm_medium=organic&utm_campaign=orderaction"
+                    >
+                      <div className="loc_pic_sq deliveroo d-flex flex-column align-items-center gap-2 p-3">
+                        <div className="icondiv d-flex align-items-center justify-content-center">
+                          <img
+                            alt="deliveroo"
+                            data-src="../images/just-eat.png"
+                            className="deliver-logo"
+                            src={require('../images/just-eat.png')}
+                          />
+                        </div>
+                        <h2 className="people-box-sub-title">Just Eat</h2>
+                        <div className="order-btn border deliver border-2 white p-2">
+                          Order now
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
