5f69f46c1ed695763d387067d796e13d7006815d
petekwok <pete.kwok@gmail.com>
2022-11-30 12:47:10 +1100

    Changed logo and coloring

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -9,13 +9,10 @@ const Location: React.FC<{}> = () => {
             <div className="col-lg-12 mob_show">
               <div className="noborder d-flex flex-column align-items-center mb-4 mt-4">
                 <h2>Steve's Fish and Chips</h2>
-                <span className="d-flex flex-column align-items-center mb-4">
-                  M - T: 11.00am to 11.00pm
-                  <br />
-                  F - S: 11.00am to 11.30pm
-                  <br />
-                  S: 11.00am to 10.30pm
-                  <br />
+                <span className="d-flex flex-column align-items-center justify-content-center mb-4 gap-2">
+                  <div>M - T: 11.00am to 11.00pm</div>
+                  <div>F - S: 11.00am to 11.30pm</div>
+                  <div>S: 11.00am to 10.30pm</div>
                   <a href="tel:019 0323 8837">019 0323 8837</a>
                   Downlands Parade, 3 Upper Brighton Rd, Worthing BN14 9JH
                   <br />
